1ba9a14bda1d971b12b38f1336b1d0f264dc254a
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-01-17 16:51:29 +0530

    2nd screen

--- simple_app_project/lib/main.dart
+++ simple_app_project/lib/main.dart
@@ -35,40 +35,125 @@ class MyApp extends StatelessWidget {
                 ),
               ),
             ]),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0), //add a padding
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment
-                .start, //Cros axis alignmnet / column alignment
-            children: [
-              const Text(
-                'SPACE DETAILS',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
+        body: SingleChildScrollView(
+          //Make scrollable the column
+          child: Padding(
+            padding: const EdgeInsets.all(8.0), //add a padding
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment
+                  .start, //Cros axis alignmnet / column alignment
+              children: [
+                const Text(
+                  'SPACE DETAILS',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 50),
-              Center(
-                child: Image.asset(
-                  "Assets/space1.png",
-                  height: 300, //size of the image change
-                  scale: 2, //scale of the image change
+                const SizedBox(height: 50),
+                Center(
+                  child: Image.asset(
+                    "Assets/space1.png",
+                    height: 300, //size of the image change
+                    scale: 2, //scale of the image change
+                  ),
+                ), //Import an image
+                const SizedBox(
+                  height: 50,
                 ),
-              ), //Import an image
-              const SizedBox(
-                height: 50,
-              ),
-              const Text(
-                "Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum .",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w400,
+                const Text(
+                  "Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum .",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  textAlign: TextAlign.center, //Align the texts
                 ),
-                textAlign: TextAlign.center, //Align the texts
-              ),
-            ],
+                const SizedBox(
+                  height: 30,
+                ),
+                //button
+                GestureDetector(
+                  onTap: () {},
+                  child: Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.red,
+                      ),
+                      child: const Text(
+                        "SPACE DETAILS",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                //Second screen
+                const SizedBox(height: 20),
+                Center(
+                  child: Image.asset("Assets/space2.png", height: 300),
+                ),
+
+                const Text(
+                  "Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum .",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Row(
+                    //Created a row
+                    mainAxisAlignment: MainAxisAlignment
+                        .spaceAround, //Main axis alignment spapce around
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.orange,
+                        ),
+                      ),
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.purple,
+                        ),
+                      ),
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.pink,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                //Third screen----------------------------------
+
+                Image.asset("Assets/space3.png", height: 300),
+              ],
+            ),
           ),
         ),
       ),
